9a99e853cd3aa922e324b08837f1b9841d871048
dxqcloud <dxq520814@163.com>
2018-12-19 23:46:35 +0800

    add validation

--- TrafficModel.py
+++ TrafficModel.py
@@ -54,6 +54,31 @@ class Model(nn.Module):
         return out
 
 
+def validation(test_data, model, args):
+    with torch.no_grad():
+        total = 0
+        y_pred = []
+        y_label = []
+        for x, type, y in test_data:
+            x = x.type("torch.FloatTensor")
+            type = type.type("torch.FloatTensor")
+            if args.use_gpu:
+                x = x.cuda()
+                type = type.cuda()
+                y = y.cuda()
+            out = model.forward(x, type)
+            total += y.size(0)
+
+            _, predicted = torch.max(out, 1)
+            y_label.extend(y.cpu().data.numpy())
+            y_pred.extend(predicted)
+
+        print("total test samples:", total)
+        print("accuracy={}%".format(100 * accuracy_score(y_label, y_pred)))
+        print(confusion_matrix(y_label, y_pred))
+        # print(classification_report(y_label, y_pred))
+
+
 def train_model(train_data, test_data, model, args):
     lossfunc = nn.CrossEntropyLoss()
     optimzer = torch.optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), lr=args.lr)
@@ -82,48 +107,24 @@ def train_model(train_data, test_data, model, args):
         # test
         print()
         print("epoch test:", epoch)
+        validation(test_data, model, args)
 
-        with torch.no_grad():
-            loss = 0
-            total = 0
-            y_pred = []
-            y_label = []
-            for x, type, y in test_data:
-                x = x.type("torch.FloatTensor")
-                type = type.type("torch.FloatTensor")
-                if args.use_gpu:
-                    x = x.cuda()
-                    type = type.cuda()
-                    y = y.cuda()
-                out = model.forward(x, type)
-                loss += lossfunc(out, y).item()
-                total += y.size(0)
+        model_path = os.path.join(args.save_path, "model.{}.pth".format(epoch))
+        torch.save(model.state_dict(), model_path)
+        print("saving model to {}".format(model_path))
 
-                _, predicted = torch.max(out, 1)
-
-                y_label.extend(y.cpu().data.numpy())
-                y_pred.extend(predicted)
-
-            print("total test samples:", total)
-            print("loss:", loss / total)
-
-            print("accuracy={}%".format(100 * accuracy_score(y_label, y_pred)))
-            print(confusion_matrix(y_label, y_pred))
-            # print(classification_report(y_label, y_pred))
-
-            model_path = os.path.join(args.save_path, "model.{}.pth".format(epoch))
-            torch.save(model.state_dict(), model_path)
-            print("saving model to {}".format(model_path))
 
 def predict_model(test_data, model, args):
 
     with torch.no_grad():
         y_pred = []
-        for x in test_data:
+        for x, type in test_data:
             x = x.type("torch.FloatTensor")
+            type = type.type("torch.FloatTensor")
             if args.use_gpu:
                 x = x.cuda()
-            out = model.forward(x)
+                type = type.cuda()
+            out = model.forward(x, type)
             _, predicted = torch.max(out, 1)
             y_pred.extend(predicted)
 
@@ -156,6 +157,8 @@ def train(args):
     print("Test data:", len(test_loader))
     print("Build Model......")
     model = Model(args.num_class)
+    if args.pretrain_model:
+        model.load_state_dict(args.pretrain_model)
 
     if args.use_gpu:
         model = model.cuda()
@@ -174,7 +177,9 @@ def predict(args):
     print("Test data:", len(test_loader))
     print("Build Model......")
     model = Model(args.num_class)
-    model.load_state_dict(args.model)
+
+    if args.pretrain_model:
+        model.load_state_dict(args.pretrain_model)
 
     if args.use_gpu:
         model = model.cuda()
@@ -194,7 +199,7 @@ if __name__ == "__main__":
     paser.add_argument("--xml_path", type=str, required=True)
     paser.add_argument("--mode", type=str, default="train", choices=["train", "test"])
     paser.add_argument("--save_path", type=str, default="model")
-    paser.add_argument("--model", type=str, default="")
+    paser.add_argument("--pretrain_model", type=str, default="")
     paser.add_argument("--batch_size", type=int, default=64)
     paser.add_argument("--worker", type=int, default=4)
     paser.add_argument("--epochs", type=int, default=10)
